6cfa4d436fac82d294b0db1baac09ab8ab6435fc
Steven Meyers <sjjmeyers@gmail.com>
2020-05-19 10:10:00 -0600

    Working on API response

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,8 @@
 class SearchController < ApplicationController
   def index
+    house = params[:house]
     
+    response = conn.get('https://www.potterapi.com/v1/characters/?key=$2a$10$A7Mo98EdH1RhntinPC12duKbhkP/yTdwlA8OXqFMpLcInDkJKWvnW&house=Gryffindor')
+    binding.pry
   end
 end
