80a81a7a50867767f477dd4172ac64ce0e7aa577
Steven Meyers <sjjmeyers@gmail.com>
2020-05-19 10:54:40 -0600

    Change object mapping in search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,7 +6,7 @@ class SearchController < ApplicationController
 
     response = conn.get("characters/?key=#{ENV['KEY']}&house=#{house}")
     json = JSON.parse(response.body, symbolize_names: true)
-    @order_of_phoenix_members = json.map do |data|
+    @members = json.map do |data|
       Member.new(data)
     end
   end
