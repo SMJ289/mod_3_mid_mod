5619ca2770bfa4c2b496c3f2c7f7fa2f11b23726
Steven Meyers <sjjmeyers@gmail.com>
2020-05-19 09:46:32 -0600

    Expand search test

--- spec/features/user_can_search_for_house_members_spec.rb
+++ spec/features/user_can_search_for_house_members_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe "As a user" do
 
     expect(current_path).to eq('/search')
   end
+
+  it 'I click search for members and see the total number of the order of the phoenix members that belong to Gryffindor' do
+    visit '/'
+
+    select "Gryffindor", from: :house
+
+    click_on "Search For Members"
+
+    expect(page).to have_css(".member", count: 21)
+  end
 end
