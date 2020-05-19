215f956f595ae8f845fb9798ab5885aeea722641
Steven Meyers <sjjmeyers@gmail.com>
2020-05-19 10:53:15 -0600

    Add order of phoenix to Member model

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,9 +1,10 @@
 class Member
-  attr_reader :name, :role, :house, :patronus
+  attr_reader :name, :role, :house, :patronus, :order_of_phoenix
   def initialize(attributes)
     @name = attributes[:name]
     @role = attributes[:role]
     @house = attributes[:house]
     @patronus = attributes[:patronus]
+    @order_of_phoenix = attributes[:orderOfThePhoenix]
   end
 end
