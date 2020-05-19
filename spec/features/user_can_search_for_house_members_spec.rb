0f2f524b67c60d59da7d4c74a4bfbed3e824191c
Steven Meyers <sjjmeyers@gmail.com>
2020-05-19 09:41:35 -0600

    Add search test

--- spec/features/user_can_search_for_house_members_spec.rb
+++ spec/features/user_can_search_for_house_members_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "As a user" do
   it 'I can select Gryffindor from the dropdown menu at the root directory' do
-    
+    visit '/'
+
+    select "Gryffindor", from: :house
+
+    click_on "Search For Members"
+
+    expect(current_path).to eq('/search')
   end
 end
